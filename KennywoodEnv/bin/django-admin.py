5169934604d3b86e547944fe140ca3496aff6fe7
Jack Parsons <jcksnparsons@yahoo.com>
2020-05-19 15:11:42 -0500

    initial commit, completed up to chapter 7

---
+++ KennywoodEnv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jcksnparsons/workspace/python/django/kennywood/KennywoodEnv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
